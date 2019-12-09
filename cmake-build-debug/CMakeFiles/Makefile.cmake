4353496d1ca28def2e12aaa3a128559b8786adbf
Tobias Wilfert <s0181439@ad.ua.ac.be>
2019-12-09 19:03:24 +0100

    Added Simple SFML test

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -37,6 +37,11 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/Users/tobiaswilfert/Library/Application Support/JetBrains/Toolbox/apps/CLion/ch-0/192.6817.32/CLion.app/Contents/bin/cmake/mac/share/cmake-3.15/Modules/Platform/Darwin.cmake"
   "/Users/tobiaswilfert/Library/Application Support/JetBrains/Toolbox/apps/CLion/ch-0/192.6817.32/CLion.app/Contents/bin/cmake/mac/share/cmake-3.15/Modules/Platform/UnixPaths.cmake"
   "/Users/tobiaswilfert/Library/Application Support/JetBrains/Toolbox/apps/CLion/ch-0/192.6817.32/CLion.app/Contents/bin/cmake/mac/share/cmake-3.15/Modules/ProcessorCount.cmake"
+  "/usr/local/lib/cmake/SFML/SFMLConfig.cmake"
+  "/usr/local/lib/cmake/SFML/SFMLConfigDependencies.cmake"
+  "/usr/local/lib/cmake/SFML/SFMLConfigVersion.cmake"
+  "/usr/local/lib/cmake/SFML/SFMLSharedTargets-release.cmake"
+  "/usr/local/lib/cmake/SFML/SFMLSharedTargets.cmake"
   )
 
 # The corresponding makefile is:
--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include "Stopwatch.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 Stopwatch* Stopwatch::instance = nullptr;
 
 int main()
 {
+        sf::RenderWindow window( sf::VideoMode(640,480), "Test Version");
+
+        while (window.isOpen()) {
+                sf::Event event;
+
+                // handle all events
+                while (window.pollEvent(event)) {
+                        switch (event.type) {
+                        case sf::Event::Closed:
+                                window.close();
+                                break;
+                        }
+                }
+                // update the game
+                window.clear();
+
+                // draw objects here
+                window.display();
+        }
+
         std::cout << CLOCKS_PER_SEC << std::endl;
 
         // Stopwatch *s = s->getInstance();
@@ -14,13 +36,13 @@ int main()
         std::cout << s->getElapsedTime() << std::endl;
 
         int i = 1;
-        while (i < 20) {
+        while (i < 5) {
                 if (s->getElapsedTime() > i) {
                         std::cout << i << std::endl;
                         std::cout << s->getElapsedTime() << std::endl;
                         ++i;
                 }
-                if (i == 5) {
+                if (i == 4) {
                         s->restart();
                         i++;
                 }
